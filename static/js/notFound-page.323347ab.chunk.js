6732ba44870d84d76d54d92e8ad2d394a032036d
devisd <89788799+devisd@users.noreply.github.com>
2022-08-20 19:00:19 +0000

    Deploying to gh-pages from @ devisd/05-movies@795f588b888a4ef9620b668bb00b8d072fc9ca7a 🚀

---
+++ static/js/notFound-page.323347ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_05_movies=self.webpackChunk_05_movies||[]).push([[685],{8769:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});t(2791);var r=t(184),i=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{style:{marginTop:"50px",textAlign:"center"},children:"404 LOL"})})}}}]);
+//# sourceMappingURL=notFound-page.323347ab.chunk.js.map
